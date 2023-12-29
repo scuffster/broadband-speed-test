01d9fba32a8ddb77df735307de2e1a41c543e7ad
scuffster <237091+scuffster@users.noreply.github.com>
2023-12-29 00:14:14 +0000

    influx

--- scripts/speedtest.py
+++ scripts/speedtest.py
@@ -1,10 +1,8 @@
-import os
 import re
 import subprocess
-import time
+from influxdb import InfluxDBClient
 
-response = subprocess.Popen('/usr/bin/speedtest --accept-license --accept-gdpr'
-                            , shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
+response = subprocess.Popen('/usr/bin/speedtest --accept-license --accept-gdpr', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
 
 ping = re.search('Latency:\s+(.*?)\s', response, re.MULTILINE)
 download = re.search('Download:\s+(.*?)\s', response, re.MULTILINE)
@@ -16,11 +14,20 @@ download = download.group(1)
 upload = upload.group(1)
 jitter = jitter.group(1)
 
-try:
-    f = open('../data/speedtest.csv', 'a+')
-    if os.stat('.../data/speedtest.csv').st_size == 0:
-            f.write('Date,Time,Ping (ms),Jitter (ms),Download (Mbps),Upload (Mbps)\r\n')
-except:
-    pass
-f.write('{},{},{},{},{},{}\r\n'.format(time.strftime('%m/%d/%y')
-                                       , time.strftime('%H:%M'), ping, jitter, download, upload))
+speed_data = [
+    {
+        "measurement" : "internet_speed",
+        "tags" : {
+            "host": "RaspberryPiMyLifeUp"
+        },
+        "fields" : {
+            "download": float(download),
+            "upload": float(upload),
+            "ping": float(ping),
+            "jitter": float(jitter)
+        }
+    }
+]
+client = InfluxDBClient('localhost', 8086, 'speedmonitor', 'pimylifeup', 'internetspeed')
+
+client.write_points(speed_data)
