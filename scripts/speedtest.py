7871e566a706717eeb49590db2150bfafe244cd0
scuffster <237091+scuffster@users.noreply.github.com>
2024-01-04 14:51:41 +0000

    influx

--- scripts/speedtest.py
+++ scripts/speedtest.py
@@ -1,6 +1,14 @@
-import re
-import subprocess
 from influxdb import InfluxDBClient
+import influxdb_client, os, time, re
+import subprocess
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+token = os.environ.get("INFLUXDB_TOKEN")
+org = "speedtest"
+url = "http://speedy01.local:8086"
+
+write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 
 response = subprocess.Popen('/usr/bin/speedtest --accept-license --accept-gdpr', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
 
