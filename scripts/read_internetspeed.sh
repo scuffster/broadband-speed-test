b8fb500dd6c00e5265ecbc51e58326684694a959
scuffster <237091+scuffster@users.noreply.github.com>
2024-01-05 09:13:56 +0000

    influx

--- scripts/read_internetspeed.sh
+++ scripts/read_internetspeed.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . /home/pi/.bashrc
 echo $INFLUXDB_TOKEN >> /home/pi/log/speedtest_influxdb2.log
- /usr/bin/python /home/pi/broadband-speed-test/scripts/speedtest_influxdb2.py &>> /home/pi/log/speedtest_influxdb2.log
+ /usr/bin/python /home/pi/broadband-speed-test/scripts/speedtest_influxdb2.py &>> /home/pi/log/read_internetspeed.log
